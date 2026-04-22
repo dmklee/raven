30fabb3ec04237abd25040c9ce040a43c6801b82
David Klee <dmklee15@gmail.com>
2026-04-21 22:54:08 -0400

    clean gh-pages branch

--- uferl/env_runner/base_image_runner.py
+++
@@ -1,8 +0,0 @@
-from typing import Dict
-
-class BaseImageRunner:
-    def __init__(self, output_dir):
-        self.output_dir = output_dir
-
-    def run(self, policy) -> Dict:
-        raise NotImplementedError()
